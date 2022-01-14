b5fa3998be4637ed5eaf9d3af6bf341cdc944107
Simon Gomes <simon.gomes@umma.id>
2022-01-14 16:37:10 +0600

    search bar component added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">Crealytics Frontend Challenge</div>;
+  return (
+    <div className="App app-wrapper">
+      <h3 className="text-center">Crealytics Frontend Challenge</h3>
+      <div className="search-bar-wrapper">
+        <input type="text" name="search" placeholder="search..." />
+      </div>
+    </div>
+  );
 }
 
 export default App;
